9cefbdc2f9190d31873b3ef7cc69eddd59277619
eryk <e.szczepaniec@lightpixel.io>
2024-12-05 17:05:20 +0100

    add testing logic & vitest tests - m2 / l1

--- examples/module2/lesson1/discount-validator/validator.ts
+++ examples/module2/lesson1/discount-validator/validator.ts
@@ -1,3 +1,5 @@
+import { isNumber } from 'es-toolkit/compat';
+
 export function formValidator(
   firstName: string,
   lastName: string,
@@ -9,12 +11,24 @@ export function formValidator(
     errors.push('First name is required');
   }
 
+  if (firstName.length === 0) {
+    errors.push('First name must have at least one character');
+  }
+
   if (!lastName) {
     errors.push('Last name is required');
   }
 
+  if (lastName.length === 0) {
+    errors.push('Last name must have at least one character');
+  }
+
   if (age < 0) {
     errors.push('Age must be a positive number');
+  }
+
+  if (!isNumber(age)) {
+    throw new Error('Age must be a number');
   }
 
   return errors;
