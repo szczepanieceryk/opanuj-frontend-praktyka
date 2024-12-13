dea11794e9ab31101d4a38c1e2453f74f5168db0
eryk <e.szczepaniec@lightpixel.io>
2024-12-13 15:08:14 +0100

    update Element ID's

--- examples/module1/lesson1/validate-it/index.ts
+++ examples/module1/lesson1/validate-it/index.ts
@@ -3,8 +3,8 @@ import { validate } from './validators';
 
 const validator = () => {
   const input: HTMLInputElement = document.getElementById('input')!;
-  const validateBtn: HTMLElement = document.getElementById('button')!;
-  const clearBtn: HTMLElement = document.getElementById('button2')!;
+  const validateBtn: HTMLElement = document.getElementById('validation-btn')!;
+  const clearBtn: HTMLElement = document.getElementById('cleanup-btn')!;
   const displayMessage: HTMLElement = document.getElementById('result')!;
 
   validateBtn.addEventListener('click', () => {
