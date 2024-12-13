649bbe993a7a4ed9b5ae17738fd097a6417a6ec7
eryk <e.szczepaniec@lightpixel.io>
2024-12-13 15:54:55 +0100

    Update success validate message

--- examples/module1/lesson1/validate-it/meessages.ts
+++ examples/module1/lesson1/validate-it/meessages.ts
@@ -2,4 +2,4 @@ export const MESSAGE_ERROR_INVALID_INPUT = 'Invalid - empty or non integer.';
 export const MESSAGE_ERROR_INVALID_INT =
   'Invalid number. Please enter a even int between 0 and 100.';
 export const MESSAGE_ERROR_INVALID_NUMBER = 'Invalid number';
-export const SUCCESS_MESSAGE_VALID_INT = 'Valid!';
+export const SUCCESS_MESSAGE_VALID_INT = 'Valid';
