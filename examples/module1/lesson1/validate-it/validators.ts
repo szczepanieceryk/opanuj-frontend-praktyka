9fea95c66dc9e443fd400593a066940b73b03fd8
eryk <e.szczepaniec@lightpixel.io>
2024-12-13 15:09:31 +0100

    add validation for empty input & out of range nums

--- examples/module1/lesson1/validate-it/validators.ts
+++ examples/module1/lesson1/validate-it/validators.ts
@@ -1,6 +1,7 @@
 import {
   MESSAGE_ERROR_INVALID_INT,
   MESSAGE_ERROR_INVALID_INPUT,
+  MESSAGE_ERROR_INVALID_NUMBER,
   SUCCESS_MESSAGE_VALID_INT,
 } from './meessages';
 import { NumericValidationMethod, isValidInteger } from './methods';
@@ -9,6 +10,14 @@ export const validate = (
   input: string,
   validators: NumericValidationMethod[]
 ): string => {
+  if (input === '') {
+    return MESSAGE_ERROR_INVALID_INPUT;
+  }
+
+  if (Number(input) > 100 || Number(input) < 0) {
+    return MESSAGE_ERROR_INVALID_NUMBER;
+  }
+
   if (!isValidInteger(input)) {
     return MESSAGE_ERROR_INVALID_INPUT;
   }
